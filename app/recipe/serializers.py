074d0ffe3cde9426a1df17dab0148e3c8a443186
Pascal Herms <pherms@outlook.com>
2021-09-02 20:06:56 +0200

    Implemented custum recipe detail view

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -34,6 +34,14 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
-                  'price', 'link')
+        fields = (
+            'id', 'title', 'ingredients', 'tags', 'time_minutes',
+            'price', 'link'
+        )
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
